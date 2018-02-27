c8b1b943a0459111571847bb6a1cbbeeae640329
EDUARDO LOMBARDI <eduardolomb@gmail.com>
2018-02-27 18:45:44 -0300

    Adicionados elementos de interface bem como suas constraints, e a classe responsável pelo download das informações.

---
+++ TwitchTopGames/DownloadViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DownloadViewController.swift
+//  TwitchTopGames
+//
+//  Created by Eduardo Lombardi on 27/02/18.
+//  Copyright © 2018 Eduardo Lombardi. All rights reserved.
+//
+
+import UIKit
+
+class DownloadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
