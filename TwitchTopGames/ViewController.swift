40e8774752449b881d2a490a5dcb598b96be01fb
EDUARDO LOMBARDI <eduardolomb@gmail.com>
2018-02-27 18:34:26 -0300

    Adicionada CollectionView

--- TwitchTopGames/ViewController.swift
+++ TwitchTopGames/ViewController.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource   {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+          let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "gameCell", for: indexPath)
+        return cell
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
