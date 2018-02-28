1ed9cd6c2239ab2696132d27a4ae2442d9a22ed7
EDUARDO LOMBARDI <eduardolomb@gmail.com>
2018-02-27 22:40:57 -0300

    Correções no layout e adição de imagem padrão.

--- TwitchTopGames/ViewController.swift
+++ TwitchTopGames/ViewController.swift
@@ -8,9 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource   {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout  {
+    
+    @IBOutlet weak var uiCollectionView: UICollectionView?
+    
+    let numberOfCellsPerRow: CGFloat = 2
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 50
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -19,12 +24,49 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
         
     }
     
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//        var height = self.view.frame.size.height
+//        let width = self.view.frame.size.width
+//        if (height * 0.45 < 252) {
+//            height = 252
+//        } else {
+//            height *= 0.45
+//        }
+//
+//        return CGSize(width: width/2, height:height )
+//
+//    }
+    
+
+//    (CGSize)collectionView:(UICollectionView *)collectionView
+//    layout:(UICollectionViewLayout *)collectionViewLayout
+//    sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+//
+//    CGFloat height = self.view.frame.size.height;
+//    CGFloat width  = self.view.frame.size.width;
+//    // in case you you want the cell to be 40% of your controllers view
+//    return CGSizeMake(width*0.4,height*0.4)
+//    }
+//
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let flowLayout = uiCollectionView?.collectionViewLayout as? UICollectionViewFlowLayout {
+            let height = self.view.frame.size.height
+            
+            let horizontalSpacing = flowLayout.scrollDirection == .vertical ? flowLayout.minimumInteritemSpacing : flowLayout.minimumLineSpacing
+            let cellWidth = (view.frame.width - max(0, numberOfCellsPerRow - 1)*horizontalSpacing)/numberOfCellsPerRow
+            flowLayout.itemSize = CGSize(width: cellWidth, height:height*0.5)
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
